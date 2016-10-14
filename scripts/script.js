a0e4f7a2105ac7761e7a23e42be5d997cad784e0
Ivan S <v3lvetnoise@gmail.com>
2016-10-14 16:36:36 +0700

    Display solution as hidden div

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,17 @@
 'use strict'
 
-function run_solution_1() {
+function show_solution_1() {
   var sum = 0;
   for (var i = 0; i <= 1000; i++) {
     if ( i % 3 == 0 || i % 5 == 0 ) {
       sum += i;
     }
   }
-  alert("The sum of all numbers dividable by 3 or 5 is: " + sum);
+  var message = "The sum of all numbers dividable by 3 or 5 is: " + sum;
+  display_solution("solution_1", message);
 }
 
-function run_solution_2() {
+function show_solution_2() {
   var sum = 0;
   var fib = [];
   fib[0] = 0;
@@ -25,6 +26,14 @@ function run_solution_2() {
     fib[1] += fib[0];
     fib[0] = fib[2];
   }
+  var message = 'The sum of all even numbers (which are not exceed four million) of Fibonacci sequence is: ' + sum;
+  display_solution("solution_2", message);
+}
 
-  alert(sum);
+function display_solution(section_id, message) {
+  var section = document.getElementById(section_id);
+  var element = document.createElement('p');
+  element.innerHTML = message;
+  section.appendChild(element);
+  section.style.display = "block";
 }
